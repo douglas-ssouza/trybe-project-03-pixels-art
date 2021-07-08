659e877e83f055f61eed8948e2be35f1532ab01a
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-08 18:05:57 -0300

    Adiciona requisitos bonus

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const palette = document.getElementById('color-palette');
 const board = document.getElementById('pixel-board');
-const colors = palette.children;
 const btnClear = document.getElementById('clear-board');
+const btnGenerate = document.getElementById('generate-board');
 let paint = 'black';
 
 window.onload = function() {
   addColors();
-  addPixels();
+  addPixels(5);
 }
 
 function createColor() {
@@ -39,11 +39,12 @@ function createPixel() {
   return newPixel;
 }
 
-function addPixels() {
-  for (let index = 0; index < 25; index += 1) {
+function addPixels(tamanho) {
+  for (let index = 0; index < tamanho * tamanho; index += 1) {
     let newPixel = createPixel();
     board.appendChild(newPixel);
   }
+  board.style.width = (42 * tamanho) + 'px';
 }
 
 function changeClassSelected() {
@@ -64,8 +65,32 @@ function paintPixel(event) {
 }
 board.addEventListener('click', paintPixel);
 
-btnClear.addEventListener('click', function () {
+btnClear.addEventListener('click', function() {
   for (let pixel of board.children) {
     pixel.style.backgroundColor = 'white';
   }
+});
+
+function clearBoard() {
+  for (let index = board.children.length - 1; index >= 0; index -= 1) {
+    board.removeChild(board.children[index]);
+  }
+}
+
+btnGenerate.addEventListener('click', function() {
+  let tamanho = document.getElementById('board-size').value;
+  
+  if (tamanho >= 1) {
+    clearBoard();
+    if (tamanho <= 5) {
+      addPixels(5);
+    } else if (tamanho >= 50) {
+      addPixels(50);
+    } else {
+      addPixels(tamanho);
+    }
+  } else {
+    alert('Board inválido!');
+  }
+
 });
